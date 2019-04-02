3edbf3437c06e266b958d5ff347258d3eed495f9
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-04-02 16:13:43 +0300

    Tweet parsing function has updated.

--- main.py
+++ main.py
@@ -7,47 +7,45 @@ auth = tweepy.OAuthHandler('EQxFiVurRxZRKZzH4tJ2PtsX8', '59WSgueMvx7VUbuYywwC6rq
 auth.set_access_token("349586645-4e7WmYpjvzKUmsKh3C9pNyv0QzlbVB80nlvR4q02", "qblYvvigttmP5elDFMsIJacO7gOknN794ubMThlyV0pfj")
 api = tweepy.API(auth)
 
-
-def getTweets(userId):
-    user=api.get_user(userId)
-    if user.protected:
-        print("User is protected")
-        return ""
-    timeline=api.user_timeline(screen_name=userId,count=5,tweet_mode="extended")
-    tweetTexts=[]
-    followerCount=user.followers_count
-    followingCount=user.friends_count
-    tweetCount=user.statuses_count
-    for tweet in timeline:  #taking tweet texts
-        if 'retweeted_status' in tweet._json:   #getting full text for retweets
-            tweetTexts.append(tweet._json['retweeted_status']['full_text'])
-        else:
-            tweetTexts.append(tweet.full_text)  #getting full text for self tweets
-    tweetDates=[[tweet.created_at]for tweet in timeline]#taking tweet dates
-    imageUrls=[]
-    for tweet in timeline:
-        if 'media' in tweet.entities:
-            for media in tweet.extended_entities['media']:
-                imageUrls.append(media['media_url'])
-        else:
-            imageUrls.append("")
-    dates=[]
-    for t in tweetDates:
-        for i in t:
-            date=str(i).split(" ")[0].split("-")
-            dates.append(date[2]+"."+date[1]+"."+date[0])
-    profileImage=user.profile_image_url
-    informations=[]
-    informations.append(tweetTexts)
-    informations.append(dates)
-    informations.append(tweetCount)
-    informations.append(followingCount)
-    informations.append(followerCount)
-    informations.append(imageUrls)
-    informations.append(profileImage)
-    # for info in informations:
-    #     print(info)
-    return informations
-
-
-getTweets('OguzGuldamlasi')
+class TweetStuff(object):
+    @staticmethod
+    def getTweets(userId):
+        user=api.get_user(userId)
+        if user.protected:
+            print("User is protected")
+            return ""
+        timeline=api.user_timeline(screen_name=userId,count=5,tweet_mode="extended")
+        tweetTexts=[]
+        followerCount=user.followers_count
+        followingCount=user.friends_count
+        tweetCount=user.statuses_count
+        for tweet in timeline:  #taking tweet texts
+            if 'retweeted_status' in tweet._json:   #getting full text for retweets
+                tweetTexts.append(tweet._json['retweeted_status']['full_text'])
+            else:
+                tweetTexts.append(tweet.full_text)  #getting full text for self tweets
+        tweetDates=[[tweet.created_at]for tweet in timeline]#taking tweet dates
+        imageUrls=[]
+        for tweet in timeline:#taking image urls of tweets's if it has
+            if 'media' in tweet.entities:
+                for media in tweet.extended_entities['media']:
+                    imageUrls.append(media['media_url'])
+            else:
+                imageUrls.append("")
+        dates=[]
+        for t in tweetDates: #string manipulation for better looking  (DD/MM/YYYY)
+            for i in t:
+                date=str(i).split(" ")[0].split("-")
+                dates.append(date[2]+"."+date[1]+"."+date[0])
+        profileImage=user.profile_image_url
+        informations=[]#Adding all informations to the informations array
+        informations.append(tweetTexts)##zerothfirst index tweet texts(contains an array)
+        informations.append(dates)##first index dates(contains an array)
+        informations.append(tweetCount)##second index (tweet count only an integer)
+        informations.append(followingCount)##third index(following count an integer)
+        informations.append(followerCount)##fourth index(followerCount an integer)
+        informations.append(imageUrls)##fifth index(image Urls an array)
+        informations.append(profileImage)
+        # for info in informations:
+        #     print(info)
+        return informations
